3bca76afb493ec7fdb191dc30a53bf45ccdc9615
souradipp76 <souradipp76@users.noreply.github.com>
2024-04-14 05:31:13 +0000

    ✅ Ready to clone and code.

--- doc_generator/base.py
+++ doc_generator/base.py
@@ -1,7 +1,7 @@
 """
-project_name base module.
+doc_generator base module.
 
-This is the principal module of the project_name project.
+This is the principal module of the doc_generator project.
 here you put your main classes and objects.
 
 Be creative! do whatever you want!
@@ -14,4 +14,4 @@ and then choose `flask` as template.
 """
 
 # example constant variable
-NAME = "project_name"
+NAME = "doc_generator"
